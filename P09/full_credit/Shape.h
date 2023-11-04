870ad123c4bd9f76612c59482e6fde834bd50391
axr5493@mavs.uta.edu <axr5493@mavs.uta.edu>
2023-11-04 18:36:46 -0500

    full credit

---
+++ P09/full_credit/Shape.h
@@ -0,0 +1,15 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <iostream>
+#include <string>
+
+class Shape{
+    public:
+        virtual std::string name() const;
+        virtual double area() const;
+        std::string to_string() const;
+
+};
+
+#endif
