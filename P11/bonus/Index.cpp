2fcd3426c639d06311ca65272a778b353bfd48c8
axr5493@mavs.uta.edu <axr5493@mavs.uta.edu>
2023-11-20 14:51:35 -0600

    bonus

---
+++ P11/bonus/Index.cpp
@@ -0,0 +1,22 @@
+
+#include "Index.h"
+
+void Index::add_word(const std::string& filename, int line, const Index::Word& word) {
+    Locations& locations = _index[word];
+    locations.emplace(filename, line);
+}
+
+std::ostream& operator<<(std::ostream& os, const Index& index) {
+    for (const auto& entry : index._index) {
+        Location::next_word(); // Reset last_filename for each new word
+        os << entry.first << ": ";
+        for (const auto& location : entry.second) {
+            os << location << ", ";
+        }
+        os << '\n';
+    }
+    return os;
+}
+
+
+
